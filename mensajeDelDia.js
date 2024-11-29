cfe7f1e1022e94edc08062a05956f1da9198f7bc
Alejandro Navarro Fernández <146034573+alejanf2885@users.noreply.github.com>
2024-11-29 12:16:18 +0100

    Class tiendas
    Clases necesarias para el funcionamiento de la tienda

---
+++ mensajeDelDia.js
@@ -0,0 +1,9 @@
+class MessageOfTheDay {
+    constructor({ title, body, image }) {
+      this.title = title;
+      this.body = body;
+      this.image = image;
+    }
+  }
+
+export default MessageOfTheDay;
